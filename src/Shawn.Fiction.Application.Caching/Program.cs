c28eaec847ac1b596f787d711869d0677e07e08a
ShawnPan <1329412398@qq.com>
2021-01-25 13:58:42 +0800

    修改

---
+++ src/Shawn.Fiction.Application.Caching/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shawn.Fiction.Application.Caching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
